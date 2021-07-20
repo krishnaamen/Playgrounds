aaa6193cb1e6ffca2f552ac34616bedd448ea0cc
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-07-20 20:56:34 +0200

    20-07-2021

--- IOS Development.playground/Contents.swift
+++ IOS Development.playground/Contents.swift
@@ -68,3 +68,93 @@ for num in newArray{
 print(temArray)
 
 // MARK: Day 2 20/07/2021 Set,Dictionary,Touples,optionals,if let etc.
+//set
+
+var myNames: Set<String> = ["Krishna","Shyam","Krishna","Shyam","Ram"]
+print(myNames)
+let newSet = Set(myArray)
+print(newSet)
+var arr1: [String] = ["1", "2", "3", "1", "2", "3","19","45"]
+print(Array(Set(arr1)))
+
+// Dictionary
+var dict: [String: String] = [:]
+dict["Name"] = "Krishna"
+dict["Address"] = "Copenhagen"
+print(dict)
+
+var dict1: [String : Int] = ["Math" : 20 , "English" : 22, "Science" : 25]
+print(dict1)
+print(dict1["Math"]!)
+print(dict1.keys)
+print(dict1.values)
+print(dict)
+dict = [:]
+print(dict)
+dict1.removeValue(forKey: "English")
+print(dict1)
+dict1["Math"] = 100
+print(dict1)
+
+
+//nill-collapsing operator
+let myName:String? = nil
+print(myName ?? "not fount")
+
+var dictArray: [String:[String]] = [
+    "First":["Ram","Shyam","Hari","Sukrish","Govinda"],
+    "Second":["1","2","3","4","5","6","7","8"]
+    
+]
+print(dictArray["First"]!)
+
+var testDict:[String:String] = [
+    "Krishna": "38",
+    "Shuva" : "35",
+    "Sukrish" : "9",
+    "Sukriti" : "2"
+]
+for(key,value) in testDict{
+    print("Key \(key) and Value \(value)")
+}
+
+
+//MARK:Tuple
+let fullName = ("Krishna", "Khanal")
+let(fname, lname) = ("Prakriti","Khanal")
+print(fullName.0)
+print(fullName.1)
+
+//_____________________________________________________________________________________________________
+
+
+//MARK: Task1
+
+print(testDict)
+for(key,value) in testDict{
+    print("[\(key),\(value)]")
+}
+// source of inspiration: https://stackoverflow.com/questions/43718476/dictionary-fetch-all-keys-and-use-them-as-array
+let keys: Array<String> = Array<String>(testDict.keys)
+print(keys)
+let values: Array<String> = Array<String>(testDict.values)
+print(values)
+//_____________________________________________________________________________________________________
+// MARK: Task2
+
+var complexDict:[String:[String:Int]] = [
+    "Krishna": [
+        "Age": 38,
+        "HomeNumber": 6
+    ],
+    "Shuva": [
+        "Age": 35,
+        "HomeNumber": 5
+    
+    ]
+]
+print(complexDict["Krishna"]!)
+let krishnaHouseNumber = complexDict["Krishna"]?["HomeNumber"] ?? 0
+let shuvasAge = complexDict["Shuva"]?["Age"] ?? 30
+print("Krishna's House number is \(krishnaHouseNumber)")
+print("Shuva's age is \(shuvasAge)")
